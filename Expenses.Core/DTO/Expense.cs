c82ab7327e542f27159985d18c795074eadb76bd
Asiel Alvarez <asielalvarez@ufl.edu>
2021-01-29 16:55:43 -0500

    Add DTOs

---
+++ Expenses.Core/DTO/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace Expenses.Core.DTO
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+    }
+}
