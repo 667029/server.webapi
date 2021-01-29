c475d6edcff698a9ab663dc0a903169c9df2ba84
Asiel Alvarez <asielalvarez@ufl.edu>
2021-01-29 16:58:17 -0500

    Add authentication controller and services

---
+++ Expenses.Core/UserService.cs
@@ -0,0 +1,57 @@
+﻿using Expenses.Core.DTO;
+using Expenses.Core.Utilities;
+using Expenses.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Expenses.Core
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AuthenticatedUser> SignIn(User user)
+        {
+            var dbUser = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == user.Username);
+
+            if(dbUser == null || !dbUser.Password.Equals(Hash.Password(user.Password)))
+            {
+                throw new Exception("Invalid username or password");
+            }
+
+            return new AuthenticatedUser()
+            {
+                Username = user.Username,
+                Token = JwtToken.GenerateAuthToken(user.Username),
+            };
+        }
+
+        public async Task<AuthenticatedUser> SignUp(User user)
+        {
+            var checkUsername = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Username.Equals(user.Username));
+
+            if (checkUsername != null)
+            {
+                throw new Exception("Username already exists");
+            }
+
+            user.Password = Hash.Password(user.Password);
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return new AuthenticatedUser
+            {
+                Username = user.Username,
+                Token = JwtToken.GenerateAuthToken(user.Username)
+            };
+        }
+    }
+}
---
+++ Expenses.WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using Expenses.Core;
+using Expenses.DB;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Expenses.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthenticationController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] User user)
+        {
+            var result = await _userService.SignUp(user);
+            return Created("", result);
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn([FromBody] User user)
+        {
+            var result = await _userService.SignIn(user);
+            return Ok(result);
+        }
+    }
+}
