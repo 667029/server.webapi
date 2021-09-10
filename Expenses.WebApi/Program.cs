46dc7d2ff371865c45d2156e377c08c7a453a5cc
Asiel Alvarez <asielalvarez@ufl.edu>
2021-09-09 21:17:20 -0400

    Enable EFC Migrations on project run

--- Expenses.WebApi/Program.cs
+++ Expenses.WebApi/Program.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Expenses.DB;
 
 namespace Expenses.WebApi
 {
@@ -13,7 +13,16 @@ namespace Expenses.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            var services = (IServiceScopeFactory)host.Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var db = services.CreateScope().ServiceProvider.GetService<AppDbContext>())
+            {
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
